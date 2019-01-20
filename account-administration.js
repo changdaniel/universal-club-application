becfd04c31ee65c8fe79599459e4a3721aee295d
changdaniel116 <changdaniel116@yahoo.com>
2019-01-20 14:58:33 -0600

    added club login and signup with corresponding tables

--- account-administration.js
+++ account-administration.js
@@ -2,17 +2,12 @@ const express = require('express')
 const app = express()
 const sqlite3 = require('sqlite3').verbose();
 
-
-//Required for login
+//Required for user login
 var email = 'email@email.com'
-var password1 = 'password'
+var id = 0;
+var password = 'password';
+//Required for club login
 
-//Required for sign up 
-var firstname = 'firstname'
-var lastname = 'lastname'
-
-//IMPLEMENT LATER CHECKING PASSWORD AND CONFIRM PASSWORD
-var password2 = 'password'
 
 //opening and checking database file
 let db = new sqlite3.Database('./accounts.db', (err) => {
@@ -35,7 +30,6 @@ db.all(sql, [], (err, rows) => {
 
 app.set('view engine', 'pug')
 
-
 //home page 
 
 app.get('/', function (request, response) {
@@ -51,12 +45,9 @@ app.get('/login', function (request, response) {
 
 app.get('/login-check', function (request, response) {
 
-  email = request.query.email;
-  password1 = request.query.password1;
+  console.log(request.query.email + ' ' + request.query.password)
 
-  console.log(email + ' ' + password1)
-
-  let sql = "SELECT Password FROM accountInfo WHERE Email = '" + email + "'";
+  let sql = "SELECT * FROM accountInfo WHERE Email = '" + request.query.email + "'";
 
   db.each(sql, [], (err, row) => {
     if (err) {
@@ -65,8 +56,16 @@ app.get('/login-check', function (request, response) {
     console.log(sql)
     console.log(row)
 
-    if (row.Password == password1) {
+    if (row.Password == password) {
       console.log('logged in!')
+      email = row.Email;
+      id = row.Userid;
+      response.redirect("/directory")
+      return;
+
+    }
+    else {
+      console.log('incorrect password')
     }
 
   })
@@ -84,10 +83,9 @@ app.get('/signup', function (request, response) {
 app.get('/signup-check', function (request, response) {
 
   email = request.query.email;
-  password1 = request.query.password1;
-  password2 = request.query.password2;
-  firstname = request.query.firstname;
-  lastname = request.query.lastname;
+  password = request.query.password;
+  var firstname = request.query.firstname;
+  var lastname = request.query.lastname;
 
 
   let sql = "SELECT Email FROM accountInfo WHERE Email = '" + email + "'";
@@ -104,21 +102,17 @@ app.get('/signup-check', function (request, response) {
     console.log(sql)
     console.log(row.Email)
 
-    response.redirect('/invalid-signup')
+    response.redirect('/invalid-signup-page')
     return;
-
-
-
   })
 
-
-  db.run(`INSERT INTO accountInfo(Email, Password, FirstName, LastName) VALUES(?, ?, ?, ?);`, (email), (password1), (firstname), (lastname), function (err) {
+  db.run(`INSERT INTO accountInfo(Email, Password, FirstName, LastName) VALUES(?, ?, ?, ?);`, (email), (password), (firstname), (lastname), function (err) {
     if (err) {
       return console.log(err.message);
     }
     console.log(`A row has been inserted`);
     response.redirect('/login')
-
+    return;
   })
 
 
@@ -133,6 +127,91 @@ app.get('/invalid-signup', function (request, response) {
  
   response.send('Name: ' + name + ' Willingness: ' + willing + ' Age: ' + age + ' Password: ' + password);
 }) */
+
+
+//club account administration
+
+app.get('/club-signup', function (request, response) {
+
+  response.render('club-signup-page')
+})
+
+app.get('/club-login', function (request, response) {
+
+  response.render('club-login-page')
+})
+
+app.get('/club-login-validation', function (request, response) {
+
+  let sql = "SELECT * FROM clubInfo WHERE ClubName = '" + request.query.clubname + "'";
+
+  db.each(sql, [], (err, row) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log(sql)
+    console.log(row)
+
+    if (row.ClubPassword == request.query.clubpassword) {
+      console.log('club logged in!')
+
+    }
+    else {
+      console.log('incorrect password')
+    }
+
+  })
+})
+
+app.get('/club-signup-validation', function (request, response) {
+
+
+
+  let sql = "SELECT ClubName FROM clubInfo WHERE ClubName = '" + request.query.clubname + "'";
+  console.log('before each method')
+  console.log(sql)
+ 
+  db.each(sql, [], (err, row) => {
+
+    console.log('after each method')
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log(sql)
+
+    response.redirect('/invalid-signup-page')
+    return;
+  })
+
+  db.run(`INSERT INTO clubInfo(ClubName, ClubPassword) VALUES(?, ?);`, (request.query.clubname), (request.query.clubpassword), function (err) {
+    if (err) {
+      return console.log(err.message);
+    }
+    console.log(`A club row has been inserted`);
+    response.redirect('/club-login')
+    return;
+  })
+
+})
+
+//directory
+
+app.get('/directory', function (request, response) {
+
+
+
+  response.render("directory-page")
+  
+
+})
+
+function directoryFetch()
+{
+  
+}
+
+
+
 
 app.listen(8080, function () {
   console.log('Server listening on port 8080');
